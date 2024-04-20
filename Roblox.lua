a8467b9170ee7b6db219e1174647e25d9167902a
LCD373 <155599541+LCD373@users.noreply.github.com>
2024-04-19 21:40:12 -0500

    Create Roblox.lua

---
+++ Roblox.lua
@@ -0,0 +1,6 @@
+getgenv().key = "Hostile"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Hosvile/The-telligence/main/MC%20KSystem%202"))()
+
+loadstring(game:HttpGet("https://github.com/MuhXd/Roblox-mobile-script/blob/main/VapeWave4.lua?raw=true"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LCD373/Scripts/main/Aim%20Script%20Assist%20Universal.lua"))();
